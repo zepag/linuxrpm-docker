ab93db47380238548e11045fece3ca6387d21e03
Pierre-Antoine Grégoire <pierre.antoine.gregoire@gmail.com>
2015-03-28 23:55:19 +0000

    Integrating the root script which should be run to start builds.

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-docker build -t "zepag/rpmbuild-centos6" rpmbuild-centos6/
-docker build -t "zepag/rpmbuild-centos7" rpmbuild-centos7/
+build_rpmbuild(){
+  cp -f run-builds.sh rpmbuild-$1/
+  pushd rpmbuild-$1
+  docker build -t "zepag/rpmbuild-$1" .
+  popd
+}
+
+build_rpmbuild centos6
+build_rpmbuild centos7
